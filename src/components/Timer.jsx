2e3aec3d8a65a686407f0b384b6d2f761ff7cff8
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-04-10 23:03:32 +0530

    Started work on timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 export default function Timer() {
     const [time, setTime] = useState(0);
-    return(
-        <h1></h1>
-    )
+    if (time === 0) {
+      return <button onClick={() => setTime(60)}>Start</button>;
+    }
+    return <div>{time}</div>;
 }
