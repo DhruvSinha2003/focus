dc843c3b35619a89b7af77a2430a84f41092504d
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-04-11 16:57:58 +0530

    Components made

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export default function Timer() {
-    const [time, setTime] = useState(0);
-    if (time === 0) {
-      return <button onClick={() => setTime(60)}>Start</button>;
-    }
-    return <div>{time}</div>;
+  const Ref = useRef(null);
+  const [duration, setDuration] = useState(0);
+  const [time, setTime] = useState("00:00:00");
+  const getTimeRemaining = (e) => {
+    const total = Date.parse(e) - Date.parse(new Date());
+    const seconds = Math.floor((total/1000)%60);
+    const minutes = Math.floor((total/1000/60)%60);
+    const hours = Math.floor((total/1000/60/60)%24);
+    return{total,hours,minutes,seconds};
+  }
 }
