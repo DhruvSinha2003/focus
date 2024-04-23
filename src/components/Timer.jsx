c9548726040c1e9124195af60e6634fc95910d08
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-04-23 21:20:41 +0530

    timer working

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Timer() {
   const [duration, setDuration] = useState(0);
@@ -16,29 +16,49 @@ export default function Timer() {
   useEffect(() => {
     let interval;
     if (timerActive && duration > 0) {
+      const endTime = new Date(Date.now() + duration * 1000);
       interval = setInterval(() => {
-        const remainingTime = getTimeRemaining(new Date(Date.now() + duration * 1000));
-        setTime(remainingTime);
+        const remainingTime = getTimeRemaining(endTime);
+        // If the timer has expired, stop the timer
+        if (remainingTime.total <= 0) {
+          clearInterval(interval);
+          setTimerActive(false);
+          setDuration(0);
+        } else {
+          setTime(remainingTime);
+        }
       }, 1000);
-    } else if (!timerActive) {
+    } else {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
   }, [timerActive, duration]);
 
+  const startTimer = (duration) => {
+    setDuration(duration);
+    setTimerActive(true);
+  };
+
+  const stopTimer = () => {
+    setTimerActive(false);
+    setDuration(0);
+  };
+
   return (
     <>
       <div className="timer">
-        {String(time.hours).padStart(2, '0')}:{String(time.minutes).padStart(2, '0')}:{String(time.seconds).padStart(2, '0')}
+        {String(time.hours).padStart(2, "0")}:
+        {String(time.minutes).padStart(2, "0")}:
+        {String(time.seconds).padStart(2, "0")}
       </div>
       <div className="start">
-        <button onClick={() => setDuration(1800)}>Start 30-min timer</button>
+        <button onClick={() => startTimer(1800)}>Start 30-min timer</button>
       </div>
       <div className="start">
-        <button onClick={() => setDuration(3600)}>Start 1-hour timer</button>
+        <button onClick={() => startTimer(3600)}>Start 1-hour timer</button>
       </div>
       <div className="start">
-        <button onClick={() => setDuration(0)}>Stop Timer</button>
+        <button onClick={stopTimer}>Stop Timer</button>
       </div>
     </>
   );
