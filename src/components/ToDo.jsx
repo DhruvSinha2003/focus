9ef4e0c81ca5f56165865aa374c67adbdf9ad82a
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-05-24 16:08:56 +0530

    Todo template

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Menu, MenuItem, Sidebar } from "react-pro-sidebar";
+import { Menu, MenuItem, Sidebar, SubMenu } from "react-pro-sidebar";
 
 export default function ToDo() {
   return (
-    <div>
-      <Sidebar>
-        <Menu>
-          <MenuItem>TO DO List</MenuItem>
-        </Menu>
-      </Sidebar>
-    </div>
+    <Sidebar>
+      <Menu>
+        <SubMenu label="Charts">
+          <MenuItem> Pie charts </MenuItem>
+          <MenuItem> Line charts </MenuItem>
+        </SubMenu>
+        <MenuItem> Documentation </MenuItem>
+        <MenuItem> Calendar </MenuItem>
+      </Menu>
+    </Sidebar>
   );
 }
