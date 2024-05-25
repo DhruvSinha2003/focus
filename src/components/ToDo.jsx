1988b318ad46d42345f030b387d9fcd64246928c
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-05-25 21:22:34 +0530

    todo

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,16 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Menu, MenuItem, Sidebar, SubMenu } from "react-pro-sidebar";
+import "./components.css";
 
 export default function ToDo() {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState(["Pie charts", "Line charts"]);
+
+  const handleInputChange = (e) => {
+    setNewItem(e.target.value);
+  };
+
+  const handleAddItem = () => {
+    if (newItem.trim() !== "") {
+      setItems([...items, newItem]);
+      setNewItem("");
+    }
+  };
+
   return (
     <Sidebar>
       <Menu>
-        <SubMenu label="Charts">
-          <MenuItem> Pie charts </MenuItem>
-          <MenuItem> Line charts </MenuItem>
+        <SubMenu label="To Do List">
+          {items.map((item, index) => (
+            <MenuItem key={index}>{item}</MenuItem>
+          ))}
         </SubMenu>
-        <MenuItem> Documentation </MenuItem>
-        <MenuItem> Calendar </MenuItem>
+        <div className="input-container">
+          <input
+            type="text"
+            value={newItem}
+            onChange={handleInputChange}
+            placeholder="Add a new to-do item"
+            className="todo-input"
+          />
+          <button onClick={handleAddItem} className="add-button">
+            Add ToDo item
+          </button>
+        </div>
       </Menu>
     </Sidebar>
   );
