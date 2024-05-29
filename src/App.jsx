c04ab8a72686343b51350662da77ae6fc3588e12
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-05-29 20:46:15 +0530

    added caretaker music

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import Background from "./components/Background";
+import MusicPlayer from "./components/MusicPlayer";
 import Timer from "./components/Timer";
 import TodoList from "./components/ToDo";
-
 function App() {
   return (
     <div className="App">
